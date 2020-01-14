abed19206da7407d9d5c9be87797c3ffd808c0c7
Troels Agergaard <TroelsAgergaard@users.noreply.github.com>
2020-01-14 13:56:45 +0100

    Add scss to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require("gulp")
 const ejs = require("gulp-ejs")
 const rename = require("gulp-rename")
+const sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
 
 
-function defaultTask(cb) {
+function html(done){
     // place code for your default task here
     gulp.src("./src/html/templates/*.ejs")
         .pipe(ejs())
@@ -11,7 +14,29 @@ function defaultTask(cb) {
             path.extname = ".html"
         }))
         .pipe(gulp.dest("./dist"))
-    cb();
-    }
+    done();
+}
 
-exports.default = defaultTask
+function watchHtml(){
+    gulp.watch("./src/html/**/*.ejs", { ignoreInitial: false}, html)
+}
+
+function scss(done){
+    gulp.src("./src/sass/*.scss")
+        .pipe(sass())
+        .pipe(rename(function(path){
+            path.extname = ".css"
+        }))
+        .pipe(gulp.dest("./dist/css"))
+    done();
+}
+
+function watchScss(){
+    gulp.watch("./src/sass/**/*.scss", { ignoreInitial: false }, scss)
+}
+
+gulp.task("dev", function(done){
+    watchHtml();
+    watchScss();
+    done();
+})
